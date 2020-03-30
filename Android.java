ed42c2bd70ff6bc4871aae2cfe29b3fdee694c0f
brucewaynecheeku <62903519+brucewaynecheeku@users.noreply.github.com>
2020-03-31 00:13:21 +0530

    Add files via upload

---
+++ Android.java
@@ -0,0 +1,21 @@
+package testLeaf_Week1Assignments;
+
+public class Android {
+	long a =400;
+	Boolean answer =true;
+	String name ="Shrikanth";
+	int b = 20;
+	float c = 20.2f;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Android obj = new Android();
+		System.out.println(obj.a);
+		System.out.println(obj.answer);
+		System.out.println(obj.name);
+		System.out.println(obj.b);
+		float c2 = obj.c;
+		System.out.println(c2);
+	}
+
+}
